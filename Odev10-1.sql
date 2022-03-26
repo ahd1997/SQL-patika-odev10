a6907e4160ac77835dced1ff3cf6bb9aa92932bc
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 12:34:34 +0300

    Update Odev10-1.sql

--- Odev10-1.sql
+++ Odev10-1.sql
@@ -1,1 +1,4 @@
-
+--dvdrental örnek veri tabanı üzerinden city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+SELECT * FROM city
+LEFT JOIN country
+ON city.country_id = country.country_id;
