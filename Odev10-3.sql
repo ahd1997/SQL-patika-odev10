101debf24d18a92effe42013c8d9addc4cdfb045
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:04:38 +0300

    Update Odev10-3.sql

--- Odev10-3.sql
+++ Odev10-3.sql
@@ -1,1 +1,4 @@
-
+--dvdrental örnek veri tabanı üzerinden customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+SELECT rental_id,first_name,last_name FROM customer
+RIGHT JOIN rental
+ON customer.customer_id = rental.customer_id
