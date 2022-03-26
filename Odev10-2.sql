77b29fbd60c217952daad6797f253f18e785fa9a
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:02:22 +0300

    Update Odev10-2.sql

--- Odev10-2.sql
+++ Odev10-2.sql
@@ -1,1 +1,4 @@
-
+--dvdrental örnek veri tabanı üzerinden customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+SELECT payment_id,customer,first_name,last_name FROM customer
+RIGHT JOIN payment
+ON customer.customer_id = payment.customer_id;
